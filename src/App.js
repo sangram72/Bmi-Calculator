ae2bc3073928fc529c92f27a142028720f92fff0
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2024-09-24 23:39:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter,Route,Router,Routes } from 'react-router-dom';
+import Nav from './Components/Nav/Nav';
 import Home from './Components/Home/Home';
+
+import Signup from './Components/Signup/Signup';
 import Login from './Components/Log/Login';
-import { useEffect, useState } from 'react';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState("false");
-
-  useEffect(() => {
-    const storedToken = localStorage.getItem("token");
-    setIsAuthenticated(storedToken !== null);
-    console.log(isAuthenticated)
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={isAuthenticated ? <Navigate to="/Home" replace /> : <Login />}
-        />
-        <Route
-          path="/Home"
-          element={isAuthenticated ? <Home /> : <Navigate to="/" replace />}
-        />
-      </Routes>
-    </BrowserRouter>
+   <BrowserRouter>
+   <Routes>
+          <Route path="/Home" element={<Home />} />
+           <Route path="/" element={<Login />} />
+
+      
+   </Routes>
+
+   </BrowserRouter>
   );
 }
 
